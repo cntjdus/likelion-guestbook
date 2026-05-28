7eda879217bbacb5aa6665bd98b8d66e2b6db60a
yeankong <chayaeeun25@gmail.com>
2026-05-29 04:51:41 +0900

    chore: 로컬 프론트 개발 서버 cors 허용

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,6 +157,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "https://likekion-guestbook.o-r.kr",
     "http://likekion-guestbook.o-r.kr",
     "http://localhost:5173",
